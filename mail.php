ca383ff1fbb69cd653ba46be558a844047a680d0
Coder Shiyar <codershiyar@gmail.com>
2022-06-19 20:33:07 +0200

    release 1

--- mail.php
+++ mail.php
@@ -3,26 +3,23 @@
 //These must be at the top of your script, not inside a function
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\SMTP;
-use PHPMailer\PHPMailer\Exception;
 
 //Load Composer's autoloader
-require 'vendor/autoload.php';
+require 'mailer/autoload.php';
 
 //Create an instance; passing `true` enables exceptions
-$mail = new PHPMailer(true);
+$mail = new PHPMailer();
 
     //Server settings
     $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
     $mail->isSMTP();                                            //Send using SMTP
-    $mail->Host       = 'host';                                 //Set the SMTP server to send through
+    $mail->Host       = 'smtp.example.com';                                 //Set the SMTP server to send through
     $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
-    $mail->Username   = 'your email';                                //SMTP username
-    $mail->Password   = 'your password';                             //SMTP password
+    $mail->Username   = 'user@example.com';                           //SMTP username
+    $mail->Password   = 'secret';                        //SMTP password
     $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
     $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
-
-    //Recipients
-    $mail->setFrom('from@example.com', 'Mailer');
+    
     //Content format
     $mail->isHTML(true);        //Set email format to HTML
     $mail->CharSet = "UTF-8"; 
