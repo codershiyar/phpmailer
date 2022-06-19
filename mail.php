1b9d2c76168ff633c8cfaeed315611f194ec68d4
Coder Shiyar <codershiyar@gmail.com>
2022-06-19 11:26:41 +0200

    ore improvement

--- mail.php
+++ mail.php
@@ -14,10 +14,10 @@ $mail = new PHPMailer(true);
     //Server settings
     $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
     $mail->isSMTP();                                            //Send using SMTP
-    $mail->Host       = 'host';                     //Set the SMTP server to send through
+    $mail->Host       = 'host';                                 //Set the SMTP server to send through
     $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
-    $mail->Username   = 'email';                     //SMTP username
-    $mail->Password   = 'password';                               //SMTP password
+    $mail->Username   = 'your email';                                //SMTP username
+    $mail->Password   = 'your password';                             //SMTP password
     $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
     $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
 
